3f43a24f2f6f22cea8105b1d55e5d4ca9ef5c3ff
Nolimet <nolimet@stamdata.nl>
2014-11-10 14:55:14 +0100

    added 3d char controler

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -3,13 +3,60 @@ using System.Collections;
 
 public class Init : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    TextMesh textM;
+    public string initText;
+
+    bool loopStarted = false;
+	void Start () 
+    {
+        textM = GetComponent<TextMesh>();
+        textM.text = "";
+        StartCoroutine(init());
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	IEnumerator init()
+    {
+        textM.text = "";
+        foreach(char c in initText)
+        {
+            textM.text += c;
+            print(c);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        StartCoroutine(loop());
+    }
+
+    IEnumerator loop()
+    {
+        bool cycle = false;
+        loopStarted = true;
+
+        string TempS = initText;
+        while (loopStarted)
+        {
+        if (cycle)
+            textM.text += "_";
+        else
+            textM.text = TempS;
+
+        cycle = !cycle;
+        yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+
+    void Update()
+    {
+        if (loopStarted && Input.GetKeyDown(KeyCode.Escape))
+        {
+            loopStarted = false;
+            StartCoroutine(init());
+        }
+    }
+
+    void OnGUI()
+    {
+       initText = GUI.TextField(new Rect(0, 0, 200, 20), initText);
+    }
 }
---
+++ Assets/Scripts/Managers/PlayerControler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour
+{
+    CharacterController control;
+    const float gravity = 20f;
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        Vector3 move = new Vector3();
+        move.x += Input.GetAxis(Axis.Horizontal)/100f;
+        move.z += Input.GetAxis(Axis.Vertical)/100f;
+        move = transform.TransformDirection(move);
+        move.y -= gravity;
+        if (Input.GetButton(Axis.Sprint))
+        {
+            move = move * 10f;
+        }
+        control.Move(move);
+    }
+}
