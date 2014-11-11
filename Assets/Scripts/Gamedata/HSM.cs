57521f4a1e7b90ad27ff7755291d77581af53a62
Nolimet <nolimet@stamdata.nl>
2014-11-11 22:09:59 +0100

    more stuff o.o

---
+++ Assets/Scripts/Gamedata/HSM.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace gameData
+{
+    public class HSM
+    {
+        //Heatlh
+        //Stamina
+        //Mana
+
+        public const int maxStamina = 100, maxHealth = 100;
+        public static float Health = 100, Stamina = 100;
+    }
+}
---
+++ Assets/Scripts/player/HSMManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HSMManager : MonoBehaviour {
+
+    //Heatlh
+    //Stamina
+    //Mana
+
+	const float staminaRegenrationRate = 4;
+
+	void Update () {
+        if (!Input.GetButton(Axis.Sprint) && gameData.HSM.Stamina < gameData.HSM.maxStamina)
+        {
+
+            if (Input.GetAxis(Axis.Horizontal) == 0 && Input.GetAxis(Axis.Vertical) == 0)
+            {
+                gameData.HSM.Stamina += staminaRegenrationRate * Time.deltaTime;
+            }
+            else
+            {
+                gameData.HSM.Stamina += (staminaRegenrationRate / 2f) * Time.deltaTime;
+            }
+            if (gameData.HSM.Stamina > gameData.HSM.maxStamina)
+                gameData.HSM.Stamina = gameData.HSM.maxStamina;
+        }
+        else if (gameData.HSM.Stamina > 1 && Input.GetButton(Axis.Sprint))
+        {
+            gameData.HSM.Stamina -= 5f * Time.deltaTime;
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.TextArea(new Rect(0,0,120,40),"stamina: " + gameData.HSM.Stamina + "/" + gameData.HSM.maxStamina);
+    }
+}
