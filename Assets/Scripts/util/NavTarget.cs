9524bfadb931720a4b1f11b19da98bd1731048d2
Nolimet <nolimet@stamdata.nl>
2014-11-12 15:14:17 +0100

    added enemy ai that only follows when is seen

--- Assets/Scripts/util/NavTarget.cs
+++ Assets/Scripts/util/NavTarget.cs
@@ -16,7 +16,19 @@ public class NavTarget : MonoBehaviour {
 
 	void Update () {
         if (agent.destination != target.position && Vector3.Distance(target.position, transform.position) < 15f)
-            Physics.RaycastAll(new Ray(transform.position, target.position));
-            agent.SetDestination(target.position);
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 100))
+            {
+                print(hit.collider.gameObject.tag);
+
+                if (hit.transform.gameObject.tag == "Player")
+                {
+                    agent.SetDestination(target.position);
+                    print("boom");
+                }
+            }
+        }
+        Debug.DrawRay(transform.position, target.position-transform.position, Color.red, 1f);
 	}
 }
