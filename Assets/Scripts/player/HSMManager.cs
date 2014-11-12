967525a8bb7c9e290e5dcbfd8253259a0ae06376
Nolimet <nolimet@stamdata.nl>
2014-11-12 12:14:32 +0100

    added a stamina bar

--- Assets/Scripts/player/HSMManager.cs
+++ Assets/Scripts/player/HSMManager.cs
@@ -9,29 +9,38 @@ public class HSMManager : MonoBehaviour {
 
 	const float staminaRegenrationRate = 4;
 
-	void Update () {
-        if (!Input.GetButton(Axis.Sprint) && gameData.HSM.Stamina < gameData.HSM.maxStamina)
-        {
+    [SerializeField]
+    Texture staminaTexture;
 
-            if (Input.GetAxis(Axis.Horizontal) == 0 && Input.GetAxis(Axis.Vertical) == 0)
+    void Update()
+    {
+        
+            if (!Input.GetButton(Axis.Sprint) && gameData.HSM.Stamina < gameData.HSM.maxStamina)
             {
-                gameData.HSM.Stamina += staminaRegenrationRate * Time.deltaTime;
+
+                if (Input.GetAxis(Axis.Horizontal) == 0 && Input.GetAxis(Axis.Vertical) == 0)
+                {
+                    gameData.HSM.Stamina += staminaRegenrationRate * Time.deltaTime;
+                }
+                else
+                {
+                    gameData.HSM.Stamina += (staminaRegenrationRate / 2f) * Time.deltaTime;
+                }
+                if (gameData.HSM.Stamina > gameData.HSM.maxStamina)
+                    gameData.HSM.Stamina = gameData.HSM.maxStamina;
             }
-            else
+        else if(Input.GetButton(Axis.Sprint))
+            if (gameData.HSM.Stamina > 1 && Input.GetButton(Axis.Sprint))
             {
-                gameData.HSM.Stamina += (staminaRegenrationRate / 2f) * Time.deltaTime;
+                if (Input.GetAxis(Axis.Horizontal) != 0 || Input.GetAxis(Axis.Vertical) != 0)
+                    gameData.HSM.Stamina -= 5f * Time.deltaTime;
             }
-            if (gameData.HSM.Stamina > gameData.HSM.maxStamina)
-                gameData.HSM.Stamina = gameData.HSM.maxStamina;
-        }
-        else if (gameData.HSM.Stamina > 1 && Input.GetButton(Axis.Sprint))
-        {
-            gameData.HSM.Stamina -= 5f * Time.deltaTime;
-        }
-	}
+    }
 
     void OnGUI()
     {
+        GUI.DrawTexture(new Rect(0, 0, (gameData.HSM.Stamina / gameData.HSM.maxStamina * 100), 40), staminaTexture);
         GUI.TextArea(new Rect(0,0,120,40),"stamina: " + gameData.HSM.Stamina + "/" + gameData.HSM.maxStamina);
+        
     }
 }
