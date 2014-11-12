d90f0f44b3c4073fbf828ffa75311417a9a1addc
Nolimet <nolimet@stamdata.nl>
2014-11-12 14:51:06 +0100

    played with nav mesh and made a simple AI

---
+++ Assets/Scripts/util/NavTarget.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavTarget : MonoBehaviour {
+
+
+    [SerializeField]
+    private Transform target;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+	void Update () {
+        if (agent.destination != target.position && Vector3.Distance(target.position, transform.position) < 15f)
+            Physics.RaycastAll(new Ray(transform.position, target.position));
+            agent.SetDestination(target.position);
+	}
+}
