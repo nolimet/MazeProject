d439d49fd3d510b886b573bf9ed6940d7300d577
Nolimet <nolimet@stamdata.nl>
2014-11-12 14:32:39 +0100

    added mana regen

--- Assets/Scripts/Gamedata/HSM.cs
+++ Assets/Scripts/Gamedata/HSM.cs
@@ -12,10 +12,13 @@ namespace gameData
         public const int maxStamina = 100, maxHealth = 100, maxMana = 100;
         public static float Health = 100, Stamina = 100 , Mana = 100;
 
-        public static bool canCast(int cost)
+        public static bool CastSpell(int cost)
         {
             if (Mana >= cost)
+            {
+                Mana -= cost;
                 return true;
+            }
             else
                 return false;
         }
--- Assets/Scripts/player/HSMManager.cs
+++ Assets/Scripts/player/HSMManager.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using gameData;
 
-public class HSMManager : MonoBehaviour {
+public class HSMManager : MonoBehaviour
+{
 
     //Heatlh
     //Stamina
     //Mana
 
-	const float staminaRegenrationRate = 4;
+    const float staminaRegenrationRate = 4;
+    const float manaRegenrationRate = 1;
 
     [SerializeField]
     Texture staminaTexture;
@@ -18,37 +21,58 @@ public class HSMManager : MonoBehaviour {
 
     void Update()
     {
-        
-            if (!Input.GetButton(Axis.Sprint) && gameData.HSM.Stamina < gameData.HSM.maxStamina)
-            {
-
-                if (Input.GetAxis(Axis.Horizontal) == 0 && Input.GetAxis(Axis.Vertical) == 0)
-                {
-                    gameData.HSM.Stamina += staminaRegenrationRate * Time.deltaTime;
-                }
-                else
-                {
-                    gameData.HSM.Stamina += (staminaRegenrationRate / 2f) * Time.deltaTime;
-                }
-                if (gameData.HSM.Stamina > gameData.HSM.maxStamina)
-                    gameData.HSM.Stamina = gameData.HSM.maxStamina;
-            }
-        else if(Input.GetButton(Axis.Sprint))
-            if (gameData.HSM.Stamina > 1 && Input.GetButton(Axis.Sprint))
-            {
-                if (Input.GetAxis(Axis.Horizontal) != 0 || Input.GetAxis(Axis.Vertical) != 0)
-                    gameData.HSM.Stamina -= 5f * Time.deltaTime;
-            }
+        stamina();
+        magic();
     }
 
     void OnGUI()
     {
-        GUI.DrawTexture(new Rect(0, 0, (gameData.HSM.Stamina / gameData.HSM.maxStamina * 120), 20), staminaTexture);
-        GUI.TextArea(new Rect(0,0,120,20),"stamina: " + Mathf.FloorToInt(gameData.HSM.Stamina) + "/" + gameData.HSM.maxStamina);
-        GUI.DrawTexture(new Rect(0, 20, (gameData.HSM.Health / gameData.HSM.maxHealth * 120), 20), healthTexture);
-        GUI.TextArea(new Rect(0, 20, 120, 20), "health: " + Mathf.FloorToInt(gameData.HSM.Health) + "/" + gameData.HSM.maxHealth);
-        GUI.DrawTexture(new Rect(0, 40, (gameData.HSM.Mana / gameData.HSM.maxMana * 120), 20), manaTexture);
-        GUI.TextArea(new Rect(0, 40, 120, 20), "mana: " + Mathf.FloorToInt(gameData.HSM.Mana) + "/" + gameData.HSM.maxMana);
-        
+        GUI.DrawTexture(new Rect(0, 0, (HSM.Stamina / HSM.maxStamina * 120), 20), staminaTexture);
+        GUI.TextArea(new Rect(0, 0, 120, 20), "stamina: " + Mathf.FloorToInt(HSM.Stamina) + "/" + HSM.maxStamina);
+        GUI.DrawTexture(new Rect(0, 20, (HSM.Health / HSM.maxHealth * 120), 20), healthTexture);
+        GUI.TextArea(new Rect(0, 20, 120, 20), "health: " + Mathf.FloorToInt(HSM.Health) + "/" + HSM.maxHealth);
+        GUI.DrawTexture(new Rect(0, 40, (HSM.Mana / HSM.maxMana * 120), 20), manaTexture);
+        GUI.TextArea(new Rect(0, 40, 120, 20), "mana: " + Mathf.FloorToInt(HSM.Mana) + "/" + HSM.maxMana);
+
+    }
+
+    void magic()
+    {
+        if (HSM.Mana < HSM.maxMana)
+        {
+            HSM.Mana += manaRegenrationRate * Time.deltaTime;
+            if (HSM.Mana > HSM.maxMana)
+                HSM.Mana = HSM.maxMana;
+        }
+
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                if (HSM.CastSpell(20))
+                    print("casted a spell");
+            }
+    }
+
+    void stamina()
+    {
+        if (!Input.GetButton(Axis.Sprint) && HSM.Stamina < HSM.maxStamina)
+        {
+
+            if (Input.GetAxis(Axis.Horizontal) == 0 && Input.GetAxis(Axis.Vertical) == 0)
+            {
+                HSM.Stamina += staminaRegenrationRate * Time.deltaTime;
+            }
+            else
+            {
+                HSM.Stamina += (staminaRegenrationRate / 2f) * Time.deltaTime;
+            }
+            if (HSM.Stamina > HSM.maxStamina)
+                HSM.Stamina = HSM.maxStamina;
+        }
+        else if (Input.GetButton(Axis.Sprint))
+            if (HSM.Stamina > 1 && Input.GetButton(Axis.Sprint))
+            {
+                if (Input.GetAxis(Axis.Horizontal) != 0 || Input.GetAxis(Axis.Vertical) != 0)
+                    HSM.Stamina -= 5f * Time.deltaTime;
+            }
     }
 }
