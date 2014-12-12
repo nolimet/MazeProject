26fb853c9909b43bac762a8d83fa8f98f8c1d134
Nolimet <nolimet@stamdata.nl>
2014-12-12 11:44:40 +0100

    began work on death Spikes and new menu

---
+++ Assets/Scripts/Objects/SpikeTrap.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Mechanismes
+{
+    public class SpikeTrap : MonoBehaviour
+    {
+
+        [SerializeField]
+        Transform[] Spikes = new Transform[0];
+        [SerializeField]
+        float spikeLenght = 2.9f;
+        [SerializeField]
+        float damage;
+        [SerializeField]
+        gameData.Stats.DMGTypes[] dmgTypes;
+
+        int l;
+        void Start()
+        {
+            l = Spikes.Length;
+            print(transform.forward);
+        }
+        void Update()
+        {
+            RaycastHit hit;
+            for (int i = 0; i < l; i++)
+            {
+                if (Physics.Raycast(Spikes[i].position, Spikes[i].forward, out hit, spikeLenght))
+                {
+                    hit.collider.transform.SendMessage("TakeDMG", new gameData.Stats.dmgData(damage, dmgTypes), SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+}
