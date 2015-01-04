d89c8159bb2a380da4e5ba892608d2f99f2c507a
Nolimet <nolimet@stamdata.nl>
2015-01-04 17:10:20 +0100

    forgot Some files

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace managers
+{
+    public class EventManager : MonoBehaviour
+    {
+
+        public delegate void PauseAction();
+        public static event PauseAction Pause;
+        public static void FirePause()
+        {
+            if (_enabledLoging)
+                print("Paused game");
+
+            if (Pause != null)
+                Pause();
+        }
+
+        public delegate void SaveAction(string filePath);
+        public static event SaveAction Save;
+        public static void SaveGame(string filePath)
+        {
+            Save(filePath);
+        }
+
+        public delegate void LoadAction(string filePath);
+        public static event LoadAction Load;
+        public static void LoadGame(string filePath)
+        {
+            Load(filePath);
+        }
+
+        public delegate void CastSpellAction(MagicSpells.SelfSpells spellSelf);
+        public static event CastSpellAction CastSpell;
+        public static void CastedSpell(MagicSpells.SelfSpells SpellSelf)
+        {
+            CastSpell(SpellSelf);
+        }
+
+        private static bool _enabledLoging = false;
+        protected static bool EventManagerExists;
+        public bool enableLoging = false;
+        void Start()
+        {
+            _enabledLoging = enableLoging;
+            if (EventManagerExists)
+                Debug.LogError("there are two EventManagers");
+            else
+                EventManagerExists = true;
+        }
+    }
+}
---
+++ Assets/Scripts/player/Magic/MagicLight.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Light))]
+public class MagicLight : MonoBehaviour {
+
+    private float maxIntensity;
+    void Start()
+    {
+        light.enabled = false;
+        maxIntensity = light.intensity;
+        managers.EventManager.CastSpell +=EventManager_CastSpell;
+
+    }
+
+    private void EventManager_CastSpell(MagicSpells.SelfSpells SpellSelf)
+    {
+        if(SpellSelf == MagicSpells.SelfSpells.magicLight)
+            StartCoroutine(casting());
+    }
+
+    IEnumerator casting()
+    {
+        HSMManager.castingContinuesSpell = true;
+        light.enabled = true;
+
+        for (int i = 0; i < 15;i++ )
+        {
+            light.intensity = (maxIntensity / 15) * i;
+            yield return new WaitForEndOfFrame();
+        }
+            
+        while (Input.GetButton(Axis.CastSpellSelf) && gameData.HSM.CastSpell(3f*Time.deltaTime))
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        for (int i = 15; i > 0; i--)
+        {
+            light.intensity = (maxIntensity / 15) * i;
+            yield return new WaitForEndOfFrame();
+        }
+
+        light.enabled = false;
+        HSMManager.castingContinuesSpell = false;
+    }
+}
