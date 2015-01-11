da1c0d7f9bbf571e67b39af44e80912369c5432f
Nolimet <nolimet@stamdata.nl>
2015-01-11 22:31:58 +0100

    added forgotten items

---
+++ Assets/Scripts/Objects/ExitPortal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Mechanismes
+{
+    public class ExitPortal : BasicUse
+    {
+        [SerializeField]
+        private string LevelToOpen = "";
+
+        protected override void UseObj()
+        {
+            Application.LoadLevel(LevelToOpen);    
+        }
+    }
+}
